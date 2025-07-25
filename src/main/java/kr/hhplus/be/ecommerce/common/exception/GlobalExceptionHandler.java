275f3ba3fa15c58fe06adff7e178246b819e9b5b
Jihee <jiheelee@aift.kr>
2025-07-25 20:04:19 +0900

    feat: GlobalExceptionHandler에 SystemException, Exception 추가

--- src/main/java/kr/hhplus/be/ecommerce/common/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/hhplus/be/ecommerce/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.ecommerce.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,10 +9,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponse> handleInsufficientStock(BusinessException ex) {
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex) {
         return ResponseEntity
                 .status(ex.getStatus())
                 .body(new ErrorResponse(ex.getCode(), ex.getMessage()));
     }
 
+    @ExceptionHandler(SystemException.class)
+    public ResponseEntity<ErrorResponse> handleSystemException(SystemException ex) {
+        return ResponseEntity
+                .status(ex.getError().getStatus())
+                .body(new ErrorResponse(ex.getError().getCode(), ex.getMessage()));
+    }
+
+    // 예상하지 못한 모든 예외에 대한 방어
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("SYS-000", "알 수 없는 서버 오류가 발생했습니다."));
+    }
 }
