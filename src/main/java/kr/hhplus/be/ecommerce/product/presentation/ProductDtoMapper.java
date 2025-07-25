f688b89aef7471aab7ad292ccc278415fc72d4a8
Jihee <jiheelee@aift.kr>
2025-07-25 19:05:56 +0900

    fix: ProductDtoMapper 오류 수정

--- src/main/java/kr/hhplus/be/ecommerce/product/presentation/ProductDtoMapper.java
+++ src/main/java/kr/hhplus/be/ecommerce/product/presentation/ProductDtoMapper.java
@@ -3,15 +3,19 @@ package kr.hhplus.be.ecommerce.product.presentation;
 import kr.hhplus.be.ecommerce.product.domain.model.Product;
 import kr.hhplus.be.ecommerce.product.domain.model.ProductOption;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductDtoMapper {
 
+    @Mapping(target = "options", source = "productOptions")
     ProductDto.ProductResponse toProductResponseDto(Product source);
 
     List<ProductDto.ProductResponse> toProductResponseDtoList(List<Product> source);
 
     ProductDto.ProductOptionResponse toProductOptionResponseDto(ProductOption source);
+
+    List<ProductDto.ProductOptionResponse> toProductOptionResponseDtoList(List<ProductOption> source);
 }
