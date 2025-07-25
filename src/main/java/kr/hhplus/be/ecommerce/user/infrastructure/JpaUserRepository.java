fe18c14e69c9c603129a1925501ec26b8db2f86b
Jihee <jiheelee@aift.kr>
2025-07-25 13:45:37 +0900

    feat: 유저(잔액) 인프라 레이어 코드 작성

---
+++ src/main/java/kr/hhplus/be/ecommerce/user/infrastructure/JpaUserRepository.java
@@ -0,0 +1,52 @@
+package kr.hhplus.be.ecommerce.user.infrastructure;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import kr.hhplus.be.ecommerce.common.infrastructure.JpaRepositoryBase;
+import kr.hhplus.be.ecommerce.user.domain.model.PointTransactionEntity;
+import kr.hhplus.be.ecommerce.user.domain.model.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static kr.hhplus.be.ecommerce.user.domain.model.QPointTransactionEntity.pointTransactionEntity;
+import static kr.hhplus.be.ecommerce.user.domain.model.QUserEntity.userEntity;
+
+@Repository
+public class JpaUserRepository extends JpaRepositoryBase {
+
+    public JpaUserRepository(JPAQueryFactory queryFactory, EntityManager entityManager) {
+        super(queryFactory, entityManager);
+    }
+
+    /**
+     * 유저 정보(잔액)
+     */
+    public Optional<UserEntity> findUserById(Long userId) {
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(userEntity)
+                        .where(userEntity.userId.eq(userId))
+                        .fetchOne());
+    }
+
+    public UserEntity saveUserEntity(UserEntity userEntity) {
+        return super.save(userEntity);
+    }
+
+    /**
+     * 포인트 거래내역
+     */
+    public List<PointTransactionEntity> findByUserIdOrderByCreatedAtDesc(Long userId) {
+        return queryFactory
+                .selectFrom(pointTransactionEntity)
+                .where(pointTransactionEntity.userId.eq(userId))
+                .fetch();
+    }
+
+    public PointTransactionEntity savePointTransactionEntity(PointTransactionEntity pointTransactionEntity) {
+        return super.save(pointTransactionEntity);
+    }
+
+} 
---
+++ src/main/java/kr/hhplus/be/ecommerce/user/infrastructure/UserRepositoryImp.java
@@ -0,0 +1,43 @@
+package kr.hhplus.be.ecommerce.user.infrastructure;
+
+import kr.hhplus.be.ecommerce.user.domain.UserRepository;
+import kr.hhplus.be.ecommerce.user.domain.model.PointTransactionEntity;
+import kr.hhplus.be.ecommerce.user.domain.model.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImp implements UserRepository {
+
+    private final JpaUserRepository jpaUserRepository;
+
+    /**
+     * 사용자
+     */
+    @Override
+    public Optional<UserEntity> findUserById(Long userId) {
+        return jpaUserRepository.findUserById(userId);
+    }
+
+    @Override
+    public UserEntity save(UserEntity userEntity) {
+        return jpaUserRepository.saveUserEntity(userEntity);
+    }
+
+    /**
+     * 포인트 거래 내역
+     */
+    @Override
+    public List<PointTransactionEntity> findTransactionsByUserIdOrderByCreatedAtDesc(Long userId) {
+        return jpaUserRepository.findByUserIdOrderByCreatedAtDesc(userId);
+    }
+
+    @Override
+    public PointTransactionEntity save(PointTransactionEntity pointTransactionEntity) {
+        return jpaUserRepository.savePointTransactionEntity(pointTransactionEntity);
+    }
+}
